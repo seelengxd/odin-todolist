0e133b5133ef0e2971c1f81c15f57cba69138837
seeleng <seelengxd@gmail.com>
2021-12-24 20:55:56 +0800

    Add edit todo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { format } from 'date-fns'; 
-// console.log(format(new Date(2021, 11, 24), 'dd MMM yyyy'));
+// console.log(format(new Date(2021, 11, 24), 'yyyy-MM-dd'));
 
 class Todo{
   constructor(title, description, dueDate, priority){
@@ -19,7 +19,12 @@ class Todo{
   edit(title, description, dueDate, priority){
     if (title) this.title = title;
     if (description) this.description = description;
-    if (dueDate) this.dueDate = dueDate;
+    if (dueDate) {
+      let [year, month, day] = dueDate.split('-');
+      month--;
+      dueDate = new Date(year, month, day);
+      this.dueDate = dueDate;
+    }
     if (priority) this.priority = priority;
   }
 
@@ -48,6 +53,14 @@ class Project{
     delete this.store[id];
     console.log(this.store);
   }
+
+  editTodo(id, title, description, dueDate, priority){
+    if (id in this.store){
+      this.store[id].edit(title, description, dueDate, priority)
+      return true;
+    }
+    return false;
+  }
 }
 
 const DOMStuff = (function(){
@@ -55,6 +68,15 @@ const DOMStuff = (function(){
   const todoList = document.querySelector('#todos')
   const addFormButton = document.querySelector("#addformbutton")
   addFormButton.onclick = renderAddForm;
+  const addHandler = e => {
+    MainHandler.addTodo();
+    hideForm(e);
+  };
+
+  const editHandler = (todo, todoDOM, id) => e => {
+    MainHandler.editTodo(todo, todoDOM, id);
+    hideForm(e);
+  }
 
   function makeInput(label, type, name, options){
     const field = document.createElement('div');
@@ -87,14 +109,14 @@ const DOMStuff = (function(){
 
   }
   
-  const addForm = (function(){
+  const view = (function(){
     const view = document.createElement('div');
     view.classList.add('view');
     const form = document.createElement('form');
     const close = document.createElement('button');
     close.textContent = 'X'
     close.id = 'close' 
-    close.onclick = hideAddForm;
+    close.onclick = hideForm;
     const h2 = document.createElement('h2');
     h2.textContent = 'Add Todo';
     const title = makeInput('Title:', "text", 'title');
@@ -102,27 +124,21 @@ const DOMStuff = (function(){
     const dueDate = makeInput('Due date:', 'date', 'dueDate');
     const priority = makeInput('Priority:', 'select', 'priority', ['low', 'med', 'high'])
     const submit = document.createElement('input');
-    submit.type = 'Submit';
+    submit.type = 'submit';
     submit.value = 'Add Todo';
     form.append(close, h2, title, description, dueDate, priority, submit);
     view.append(form);
-    form.onsubmit = e => {
-      e.preventDefault();
-      MainHandler.addTodo();
-    }
-    return view;
+    form.onsubmit = addHandler;
+    return {main:view, form, h2, title, description, dueDate, priority, submit};
     
   })();
-  // const addForm = document.getElementById("addform");
-  // addForm.remove();
-  console.log(addForm);
 
   function clearElement(ele){
     while (ele.firstChild){
       ele.removeChild(ele.firstChild);
     }
   }
-  function clearTodo(){
+  function clearTodos(){
     clearElement(todoList);
   }
 
@@ -146,9 +162,12 @@ const DOMStuff = (function(){
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'delete';
     deleteButton.onclick = deleteCallback(task);
+    const viewButton = document.createElement('button');
+    viewButton.textContent = 'view';
+    viewButton.onclick = () => viewTodo(todo, task, id);
     left.classList.add('left');
     left.append(h3, date);
-    right.append(checkbox, deleteButton);
+    right.append(checkbox, deleteButton, viewButton);
     right.classList.add('right');
     task.append(left, right);
     task.classList.add(todo.priority);
@@ -156,26 +175,42 @@ const DOMStuff = (function(){
     return task;
   }
 
-  function viewTodo(todo){
+  function viewTodo(todo, todoDOM, id){
+    // title, description, dueDate, priority
+    view.h2.textContent = 'View Todo'
+    view.title.querySelector('input').value = todo.title;
+    view.description.querySelector('textarea').value = todo.description;
+    view.dueDate.querySelector('input').value = format(todo.dueDate, 'yyyy-MM-dd');
+    view.priority.querySelector('select').value = todo.priority;
+    view.submit.value = 'Edit todo'
+    view.form.onsubmit = editHandler(todo, todoDOM, id);
+    console.log(view.onsubmit);
+    document.querySelector('body').append(view.main);
 
   }
 
   function deleteTodo(todo){
-
+    todo.remove();
   }
 
   function renderAddForm(){
-    document.querySelector('body').append(addForm);
+    view.h2.textContent = 'Add Form';
+    view.submit.value = 'Add Form';
+    view.title.querySelector('input').value = '';
+    view.description.querySelector('textarea').value = '';
+    view.dueDate.querySelector('input').value = '';
+    view.priority.querySelector('select').value = '';
+    document.querySelector('body').append(view.main);
 
   }
 
-  function hideAddForm(e){
-    e.preventDefault();
-    addForm.remove();
+  function hideForm(e){
+    if (e) e.preventDefault();
+    view.main.remove();
 
   }
 
-  function extractAddForm() {
+  function extractForm() {
     const title = document.querySelector("[name='title']").value;
     const description = document.querySelector("[name='description']").value;
     const dueDate = document.querySelector("[name='dueDate']").value;
@@ -183,7 +218,7 @@ const DOMStuff = (function(){
     return {title, description, dueDate, priority};
   }
 
-  function clearAddForm(){
+  function clearForm(){
     document.querySelector("[name='title']").value = '';
     document.querySelector("[name='description']").value = '';
     document.querySelector("[name='dueDate']").value = '';
@@ -191,7 +226,15 @@ const DOMStuff = (function(){
 
   }
 
-  return { clearElement, clearTodo, addTodo, extractAddForm, clearAddForm}
+  function editTodo(todo, todoDOM){
+    todoDOM.querySelector('h3').textContent = todo.title;
+    todoDOM.querySelector('p').textContent = format(todo.dueDate, 'dd MMM yyyy');
+    todoDOM.classList.remove('low', 'med', 'high');
+    todoDOM.classList.add(todo.priority);
+
+  }
+
+  return { clearElement, clearTodos, addTodo, extractForm, clearForm, deleteTodo, editTodo, hideForm }
 })();
 
 const MainHandler = (function(){
@@ -200,23 +243,30 @@ const MainHandler = (function(){
   let currentProject = defaultProject;
 
   function addTodo(){
-    console.log(DOMStuff.extractAddForm());
-    const { title, description, dueDate, priority } = DOMStuff.extractAddForm();
-    const [index, newTodo] = currentProject.addTodo(title, description, dueDate, priority);
+    const { title, description, dueDate, priority } = DOMStuff.extractForm();
+    const [ index, newTodo ] = currentProject.addTodo(title, description, dueDate, priority);
     const deleteCallback = domTodo => e => {
       e.preventDefault();
       deleteTodo(domTodo);
     }
     DOMStuff.addTodo(newTodo, index, deleteCallback);
-    DOMStuff.clearAddForm();
+    DOMStuff.clearForm();
   }
 
   function deleteTodo(todoDOM){
     const id = todoDOM.dataset.id;
-    todoDOM.remove();
+    DOMStuff.deleteTodo(todoDOM);
     currentProject.removeTodo(id);
 
   }
 
-  return { addTodo };
+  function editTodo(todo, todoDOM, id){
+    const { title, description, dueDate, priority } = DOMStuff.extractForm();
+    if (currentProject.editTodo(id, title, description, dueDate, priority)){
+      DOMStuff.editTodo(todo, todoDOM)
+    }
+    
+  }
+
+  return { addTodo, editTodo };
 })();
